55c2f03093f5fa1e2f19f258a446c9be77c0b389
CTE/ZL/IFRN <93982613+cte-ead@users.noreply.github.com>
2024-01-24 16:52:18 -0300

    Create ava-setup-env-dev.sh

---
+++ profile/ava-setup-env-dev.sh
@@ -0,0 +1,15 @@
+# Baixe o projeto na pasta de exemplo (se for outra, basta que altere os scripts)
+mkdir -p ~/projetos/IFRN/ava
+git clone git@github.com:cte-zl-ifrn/ava_workspace.git ~/projetos/IFRN/ava/ava_workspace
+cd ~/projetos/IFRN/ava/ava_workspace
+
+# Configura o teu /etc/hosts para atender por http://ava
+./ava setup
+
+if [[ "$SHELL" == '/bin/bash' ]]; then
+    source ~/.bashrc
+fi
+
+if [[ "$SHELL" == '/bin/zsh' ]]; then
+    source ~/.zshrc
+fi
