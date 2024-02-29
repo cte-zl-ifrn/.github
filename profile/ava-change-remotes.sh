e3554b410a6df4f3e316396acaf590e3de099e64
Kelson da Costa Medeiros <144546949+kelson-medeiros-ifrn@users.noreply.github.com>
2024-02-29 20:06:42 -0300

    Create ava-change-remotes.sh

---
+++ profile/ava-change-remotes.sh
@@ -0,0 +1,17 @@
+
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/ava_workspace            && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/ava/ava_workspace.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/integration/painel-ava   && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/ava/integration/painel-ava.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/lms/moodle               && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/ava/lms/moodle.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/lms/local_suap           && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/ava/lms/local_suap.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/lms/local_pnp            && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/ava/lms/auth_suap.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/lms/enrol_suap           && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/ava/lms/enrol_suap.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/lms/auth_suap            && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/ava/lms/auth_suap.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/lms/block_suapattendance && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/ava/lms/block_suapattendance.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/lms/theme_moove          && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/ava/lms/theme_moove.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/lms/format_suap          && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/ava/lms/format_suap.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/h5p/predependencias      && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/ava/h5p/pre-dependencias.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/h5p/codehighlighter      && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/ava/h5p/codehighlighter.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/h5p/texteditor           && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/ava/h5p/codehighlighter.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/common/sass              && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/ava/common/sass.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/common/favicons          && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/ava/common/favicons.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/mobile/moodle-app        && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/ava/mobile/moodle-app.git
