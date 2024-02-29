e71649f845762a023d40f93890155d7e410840e7
Kelson da Costa Medeiros <144546949+kelson-medeiros-ifrn@users.noreply.github.com>
2024-02-29 15:01:30 -0300

    Create ava-changeremotes.sh

---
+++ profile/ava-changeremotes.sh
@@ -0,0 +1,16 @@
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/ava_workspace=git@codelab.ifrn.edu.br:dead-zl/ava/ava_workspace.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/integration/painel-ava=git@codelab.ifrn.edu.br:dead-zl/ava/integration/painel-ava.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/lms/moodle=git@codelab.ifrn.edu.br:dead-zl/ava/lms/moodle.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/lms/local_suap=git@codelab.ifrn.edu.br:dead-zl/ava/lms/local_suap.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/lms/local_pnp=git@codelab.ifrn.edu.br:dead-zl/ava/lms/auth_suap.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/lms/enrol_suap=git@codelab.ifrn.edu.br:dead-zl/ava/lms/enrol_suap.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/lms/auth_suap=git@codelab.ifrn.edu.br:dead-zl/ava/lms/auth_suap.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/lms/block_suapattendance=git@codelab.ifrn.edu.br:dead-zl/ava/lms/block_suapattendance.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/lms/theme_moove=git@codelab.ifrn.edu.br:dead-zl/ava/lms/theme_moove.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/lms/format_suap=git@codelab.ifrn.edu.br:dead-zl/ava/lms/format_suap.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/h5p/predependencias=git@codelab.ifrn.edu.br:dead-zl/ava/h5p/pre-dependencias.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/h5p/codehighlighter=git@codelab.ifrn.edu.br:dead-zl/ava/h5p/codehighlighter.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/h5p/texteditor=git@codelab.ifrn.edu.br:dead-zl/ava/h5p/codehighlighter.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/common/sass=git@codelab.ifrn.edu.br:dead-zl/ava/common/sass.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/common/favicons=git@codelab.ifrn.edu.br:dead-zl/ava/common/favicons.git
+cd "${PROJECT_DIR:-$HOME/projetos/IFRN/ava}"/mobile/moodle-app=git@codelab.ifrn.edu.br:dead-zl/ava/mobile/moodle-app.git
