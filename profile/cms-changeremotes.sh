ba4a6df7d49afe72ab0d48e8710054c59e23b079
Kelson da Costa Medeiros <144546949+kelson-medeiros-ifrn@users.noreply.github.com>
2024-02-29 13:21:56 -0300

    Update cms-changeremotes.sh

--- profile/cms-changeremotes.sh
+++ profile/cms-changeremotes.sh
@@ -1,10 +1,1 @@
 cd "${PROJECT_DIR:-$HOME/projetos/IFRN/cms}"/cms_workspace         && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/cms/cms_workspace.git
-cd "${PROJECT_DIR:-$HOME/projetos/IFRN/cms}"/plugin/suap-sso       && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/cms/plugin/suap-sso.git
-cd "${PROJECT_DIR:-$HOME/projetos/IFRN/cms}"/plugin/lean_more      && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/cms/plugin/lean_more.git
-cd "${PROJECT_DIR:-$HOME/projetos/IFRN/cms}"/theme/portal          && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/cms/theme/zl23.git
-cd "${PROJECT_DIR:-$HOME/projetos/IFRN/cms}"/theme/ajuda           && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/cms/theme/ajuda.git
-cd "${PROJECT_DIR:-$HOME/projetos/IFRN/cms}"/theme/leitor          && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/cms/theme/leitor.git
-cd "${PROJECT_DIR:-$HOME/projetos/IFRN/cms}"/theme/culturapotiguar && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/cms/theme/culturapotiguar.git
-cd "${PROJECT_DIR:-$HOME/projetos/IFRN/cms}"/theme/semead17        && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/cms/theme/semead17.git
-cd "${PROJECT_DIR:-$HOME/projetos/IFRN/cms}"/theme/semead22        && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/cms/theme/semead22.git
-cd "${PROJECT_DIR:-$HOME/projetos/IFRN/cms}"/theme/semead24        && git remote set-url origin git@codelab.ifrn.edu.br:dead-zl/cms/theme/semead24.git
