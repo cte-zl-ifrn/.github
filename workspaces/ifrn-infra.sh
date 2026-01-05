3aa647ba8cdb343f5cd304c8077562d8aad2dfd1
kelsoncm <kelsoncm@gmail.com>
2026-01-05 16:46:56 -0300

    env: reorganiza as pastas

--- workspaces/ifrn-infra.sh
+++ workspaces/ifrn-infra.sh
@@ -4,5 +4,3 @@ WPS_DIR="${WORKSPACE_DIR:-$PRJ_DIR/infra_workspace}"
 
 mkdir -p $PRJ_DIR
 git clone git@codelab.ifrn.edu.br:dead-zl/infra/infra_workspace.git $WPS_DIR
-
-code $WPS_DIR/infra_workspace.code-workspace
