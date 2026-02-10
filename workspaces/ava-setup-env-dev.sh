a47bb36489c240d2de388583d0f7c1176f0b530c
kelsoncm <kelsoncm@gmail.com>
2026-02-10 19:29:44 -0300

    Update ava-setup-env-dev.sh

--- workspaces/ava-setup-env-dev.sh
+++ workspaces/ava-setup-env-dev.sh
@@ -1,6 +1,6 @@
 # Baixe o projeto na pasta de exemplo (se for outra, basta que altere os scripts)
 mkdir -p ~/projetos/IFRN/ava
-git clone git@codelab.ifrn.edu.br:dead-zl/ava/ava_workspace.git ~/projetos/IFRN/ava/workspace
+git clone git@github.com:cte-zl-ifrn/ava_workspace.git ~/projetos/IFRN/ava/workspace
 cd ~/projetos/IFRN/ava/workspace
 
 # Configura o teu /etc/hosts para atender por http://ava
